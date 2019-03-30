424c54be1abc45fdfeae58f8a86d6f99185c423d
Oskiboy <oskar.oestby@gmail.com>
2019-03-30 10:38:21 +0100

    Changes the socket_server script.

--- socket_server.py
+++ socket_server.py
@@ -1,17 +1,25 @@
 import socket
 import argparse
 
+commands = {
+    "setMotor": [1, 0, 0, 0]
+}
 def main(args):
+    print(type(args.msg))
+    b = list()
+    for i in args.msg[:4]:
+        b.append(i)
+    b = bytes(b)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((args.ip, args.port))
-        s.sendall(" ".join(args.msg).encode('ascii'))
-        data = s.recv(1024)
-        print(data.decode())
+        s.sendall(b)
+        data = s.recv(4)
+        print(data)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--ip", default="localhost", help="Ip address to the server")
 parser.add_argument("-p", "--port", type=int, default=8080, help="Port for the socket server")
-parser.add_argument("msg", nargs="*")
+parser.add_argument("msg", type=int, nargs="+")
 args = parser.parse_args()
 
 if __name__ == '__main__':
