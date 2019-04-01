694b7bd2bd996dcfb5ec09cd9578b82206a61a7c
Oskiboy <oskar.oestby@gmail.com>
2019-04-01 10:31:14 +0200

    Removes the command line tool from main for further development

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,6 @@ int main(int argc, char** argv) {
 
     std::shared_ptr<ElevServer> e_ptr;
     std::thread server;
-    std::string cmd = "";
     std::cout << "Starting server..." << std::endl;
     try {
         e_ptr = std::make_shared<ElevServer>(6060, 1, "logs/test.log");
@@ -30,46 +29,19 @@ int main(int argc, char** argv) {
         std::cout << "Server started!" << std::endl;
     } catch(const std::exception& e) {
         std::cout << e.what() << std::endl;
+        exit(1);
     }
 
-
-    std::vector<std::string> tokens;
-    std::string token;
-    while(running) {
-        tokens.clear();
-        std::cout << ">>> ";
-        std::getline(std::cin, cmd, '\n');
-        
-        std::stringstream ss(cmd);
-        while(std::getline(ss, token, ' ')) {
-            tokens.push_back(token);
-        }
-
-        if(tokens[0] == "pos") {
-            std::cout << e_ptr->getPosition(0) << std::endl;
-        } else if(tokens[0] == "cmd") {
-            char msg[4] = {0, 0, 0, 0};
-            try{
-                for(int i = 0; i < 4; ++i) {
-                    msg[i] = std::stoi(tokens[i+1]);
-                }
-            } catch(const std::exception& e) {
-                std::cout << e.what() << std::endl;
-            }
-             
-            e_ptr->elevControl(msg);
-        } else if(tokens[0] == "q" || tokens[0] == "quit") {
-            break;
-        }
-    }
+    while(running)
+        ;
     
     std::cout << "Sending stop signal to server..." << std::endl;
     e_ptr->stop();
 
-    std::cout << "Waiting for threads to join" << std::endl;
+    std::cout << "Waiting for threads to join..." << std::endl;
     if(server.joinable()) {
         server.join();
     }
-
+    std::cout << "Server shut down!" << std::endl;
     return 0;
 }
