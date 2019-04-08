82918a7db37e782d49e7de6ef4ffd23a92caa250
Oskiboy <oskar.oestby@gmail.com>
2019-04-08 16:57:10 +0200

    Fixes too naive return on one commands.

--- wrapper/elev.c
+++ wrapper/elev.c
@@ -56,8 +56,12 @@ int write_to_socket(unsigned char _cmd[4]) {
         char buffer[4];
         bzero(&buffer, sizeof(buffer));
         read(sockfd, buffer, sizeof(buffer));
+        printf("%d-%d-%d-%d\n", buffer[0], buffer[1], buffer[2], buffer[3]);
         close(sockfd);
-        return buffer[1];
+        if(_cmd[0] == 7) {
+            return (buffer[1] ? buffer[2] : -1);
+        }
+        return (int)buffer[1];
     }
 
     close(sockfd);
